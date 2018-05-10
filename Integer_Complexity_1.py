d6e0ba47dba00735c468c5f45beaa5c75b8ebb1f
damianMietus <damianmietus1@gmail.com>
2018-05-09 23:43:39 -0400

    Add files via upload

---
+++ Integer_Complexity_1.py
@@ -0,0 +1,33 @@
+# By Damian Mietus
+# /r/DailyProgrammer
+# Completed May 9 2019
+#C hallenge #354 [Easy] Integer Complexity 1
+print("Input a number : ")
+
+a = input()
+a = int(a)
+b = 0
+c = 0
+d = 0
+tempb = 0
+tempC = 0
+tempD = 0
+count = 1
+
+while(count <= a):
+    if(a%count == 0):      
+        tempB = count
+        tempC =  a/count
+        tempD = tempB+tempC
+        if(d == 0):
+            d = tempD
+            c = tempC
+            b = tempB
+        elif(tempD < d):
+            d = tempD
+            c = tempC
+            b = tempB                                
+    count = count+1
+    
+print("The sum of the smallest pair of factors is:", d, "\nthe factors are:", c, " + ", b)
+input('Press ENTER to exit')
